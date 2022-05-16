e9a6b1f3f713526dd2c9c927489abcc6ac9f7877
DannisMa <d085770128@gmail.com>
2022-05-16 21:44:14 +0800

    finish walk
    Can walk but rotate too fast,
    and i not sure why character can't jump

--- Assets/Script/Battle Scene/PlayerControllor.cs
+++ Assets/Script/Battle Scene/PlayerControllor.cs
@@ -6,12 +6,13 @@ public class PlayerControllor : MonoBehaviour
 {
     private CharacterController characterController;
     private Animator anima;
-    private float move_speed = 0.3f;
-    private float rotate_speed = 90f;
-    [SerializeField]
-    private float jump_hight = 9.0f;
-    float gravity = 20f;
-    Vector3 move_direction = Vector3.zero, vDir = Vector3.zero;
+    private float move_speed = 4.0f;
+    private float jump_speed = 5.0f;
+    private float rotate = 180.0f;　// 旋轉速度
+    private float horizontalMove , verticalMove;
+    private Vector3 dir; //儲存移動的方向    Vector3 move_direction = Vector3.zero, vDir = Vector3.zero;
+    private float gravity = 9.81f;
+    private Vector3 velocity; //控制Y軸加速度
 
     void Awake(){
         characterController = GetComponent<CharacterController>();
@@ -27,20 +28,27 @@ public class PlayerControllor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        if (Mathf.Abs(h) > 0.01f || Mathf.Abs(v) > 0.01f) {
-            // 開啟奔跑動畫
+        if ( Input.GetKey(KeyCode.A) ){ transform.Rotate( 0, -rotate* Time.deltaTime, 0 ); }
+        if ( Input.GetKey(KeyCode.D) ){ transform.Rotate( 0, rotate* Time.deltaTime, 0 ); }
+        horizontalMove = Input.GetAxis("Horizontal") * move_speed;
+        verticalMove   = Input.GetAxis("Vertical") * move_speed;
+        dir = transform.forward * verticalMove + transform.right * horizontalMove;
+        
+        if(dir != Vector3.zero){
             anima.SetBool("Walk", true);
-            // 這裡旋轉人物使他朝向我們想移動的方向
-            transform.rotation = Quaternion.LookRotation(new Vector3(-h, 0, -v));
-            Vector3 movedir = new Vector3(-h, -20, -v);
-            // 用角色控制器讓他移動
-            characterController.Move(movedir * move_speed);
-        } else {
-            // 切換到靜止動畫
+        }
+        else{
             anima.SetBool("Walk", false);
         }
+
+        characterController.Move(dir * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && characterController.isGrounded){
+            velocity.y = jump_speed;
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 
     private void FixedUpdate(){
