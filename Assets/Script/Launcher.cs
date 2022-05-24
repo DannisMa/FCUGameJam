191cfd527c9da20066dcb49150150ed88746d231
DannisMa <d085770128@gmail.com>
2022-05-24 17:34:06 +0800

    Main Scene connect PUN2

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace com.Dannis.FCUGameJame{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        private string game_version = "1";
+        private string player_name = "";
+        [Tooltip("遊戲室玩家人數上限. 當遊戲室玩家人數已滿額, 新玩家只能新開遊戲室來進行遊戲.")]
+        [SerializeField]
+        private byte max_player_per_room = 10;
+        [SerializeField]
+        private Button battle_button;
+        [SerializeField]
+        private TMP_Dropdown battle_scene_dropdown;
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            // 確保所有玩家載入相同的遊戲場景
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            battle_button.onClick.AddListener(OnClickBattleButton);
+            battle_scene_dropdown.onValueChanged.AddListener(number => OnBattleSceneChange(number));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void Connect(){
+            // 檢查連線
+            if(PhotonNetwork.IsConnected){
+                // 已連線，隨機加入一間遊戲室
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else{
+                // 嘗試與photon cloud 連線
+                PhotonNetwork.GameVersion = game_version;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN 呼叫 OnConnectedToMaster(), 已連上 Photon Cloud.");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN 呼叫 OnDisconnected(){0}.", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN 呼叫 OnJoinRandomFailed(), 隨機加入遊戲室失敗 : "+message);
+            // 隨機加入遊戲室失敗. 可能原因是 1. 沒有遊戲室 或 2. 有的都滿了.    
+            // 因此我們自己開一個遊戲室.
+            PhotonNetwork.CreateRoom(null, new RoomOptions{MaxPlayers = max_player_per_room});
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN 呼叫 OnJoinedRoom(), 已成功進入遊戲室中.");
+        }
+
+        private void OnClickBattleButton(){
+            Connect();
+        }
+        private void OnBattleSceneChange(int num){
+            Debug.LogFormat("戰鬥場景號碼 : {0}", num);
+            if(num == 0)
+                max_player_per_room = 1;
+            else if(num == 1)
+                max_player_per_room = 2;
+            else
+                max_player_per_room = 10;
+        }
+    }
+}
--- Assets/Script/Main Scene/GameControllor.cs
+++ Assets/Script/Main Scene/GameControllor.cs
@@ -4,37 +4,43 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class GameControllor : MonoBehaviour
 {
     [SerializeField]
-    private Button battle_button;
-    [SerializeField]
-    private TMP_Dropdown battle_scene_dropdown;
+    private InputField player_name_input_field;
+    private const string player_name_pref_key = "Player Name";
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
     void Start()
     {
-        // battle_scene_dropdown.onValueChanged.AddListener(delegate{
-        //     DropdownValueChange(battle_scene_dropdown);
-        // });
-        battle_button.onClick.AddListener(ClickBattleBtn);
+        if (player_name_input_field != null)
+            player_name_input_field.onValueChanged.AddListener(name => OnPlayerNameChanged(name));
+
+        string default_name = string.Empty;
+
+        if (player_name_input_field != null)
+            player_name_input_field.text = default_name;
+        if (PlayerPrefs.HasKey(player_name_pref_key))
+        {
+            default_name =  PlayerPrefs.
+            GetString(player_name_pref_key);
+            player_name_input_field.text = default_name;
+        }
+            // 設定遊戲玩家的名稱
+        PhotonNetwork.NickName = default_name;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnPlayerNameChanged(string name){
+        if(string.IsNullOrEmpty(name))
+            return;
+        name = player_name_input_field.text;
+            // 設定遊戲玩家的名稱
+        PhotonNetwork.NickName = name;
+        PlayerPrefs.SetString(player_name_pref_key, name);
     }
-
-    private void ClickBattleBtn(){
-        if(battle_scene_dropdown.value == 0)
-            SceneManager.LoadScene("Battle Scene");
-        else
-         Debug.LogWarning(battle_scene_dropdown.value + "Scene not finish yet");
-    }
-
-    // private void DropdownValueChange(TMP_Dropdown dd){
-    //     Debug.Log(dd.value);
-    // }
 }
